b77961de656b3ec6e678ff91be55b84db68ec626
LeanN TS <leonicolas6@gmail.com>
2023-06-26 22:28:39 -0300

    Agregar archivos de proyecto.

---
+++ Datos/DatosUsuarios.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entidades;
+using System.Data.OleDb;
+
+namespace Datos
+{
+    public class DatosUsuarios : DatosConexionBD
+    {
+        public int abmUsuarios(string accion, Usuario objUsuario)
+        {
+            int resultado = -1; //verifica que se realice la operacion
+            string orden = string.Empty; //guardar consulta sql
+
+            if (accion == "Alta") // agregar usuario
+                orden = "insert into Usuarios values (" + objUsuario.CodUser +
+                ", '" + objUsuario.User + "', '" 
+                + objUsuario.ClaveUser + "', '" 
+                + objUsuario.Nombre + "', '"
+                + objUsuario.Apellido + "', '" 
+                + objUsuario.Email + "', "
+                + objUsuario.Edad + ", '" 
+                + objUsuario.FechaNacimiento + "', '" 
+                + objUsuario.FechaCreacion + "', "
+                + objUsuario.NumeroCel + "');";
+
+            if (accion == "Modificar") //Modifica propiedades del usuario existente
+            {
+                orden = "update Profesionales set clavUser='" + objUsuario.ClaveUser +
+                    "', set nomUser= '" + objUsuario.Nombre +
+                    "', set apelUser= '" + objUsuario.Apellido +
+                    "', set edadUser= " + objUsuario.Edad +
+                    ", set nacimientoUser= '" + objUsuario.FechaNacimiento +
+                    "', set numCel= " + objUsuario.NumeroCel +
+                    " where codUser= " + objUsuario.CodUser + "; ";
+            }
+
+
+            SqlCommand cmd = new SqlCommand(orden, conexion);
+
+            try
+            {
+                AbrirConexion();
+                resultado = cmd.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al tratar de guardar,borrar o modificar de Usuario",e);
+            }
+            finally
+            {
+                Cerrarconexion();
+                cmd.Dispose();
+            }
+            return resultado;
+        }
+
+        public DataSet listadoProfesionales(string cual) //1 o todos los datos
+        {
+            string orden = string.Empty;
+            if (cual != "Todos")
+                orden = "select * from Usuarios where CodProf = " + int.Parse(cual) + ";";
+            else
+                orden = "select * from Usuarios;";
+            SqlCommand cmd = new SqlCommand(orden, conexion);
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter();
+            try
+            {
+                AbrirConexion();
+                cmd.ExecuteNonQuery();
+                da.SelectCommand = cmd;//quede aca
+                da.Fill(ds);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Error al listar profesionales", e);
+            }
+            finally
+            {
+                Cerrarconexion();
+                cmd.Dispose();
+            }
+            return ds;
+        }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        #region atributos
+        private int codUser;
+        private string user;
+        private string claveUser;
+        private string nombre;
+        private string apellido;
+        private string email;
+        private int edad;
+        private DateTime fechaNacimiento;
+        private DateTime fechaCreacion;
+        private int numerocel;
+        #endregion
+
+        #region Constructor
+
+        public Usuario()
+        {
+            codUser = 0;
+            user = string.Empty;
+            nombre = string.Empty;
+            apellido = string.Empty;
+            edad = 0;
+            fechaNacimiento = DateTime.MinValue;
+            fechaCreacion = DateTime.MinValue;
+        }
+        #endregion
+        #region propiedades/encapsulamiento
+        public int CodUser
+        {
+            get { return codUser; }
+            set { codUser = value; }
+        }
+        public string User
+        {
+            get { return user; }
+            set { user = value; }
+        }
+        public string ClaveUser
+        {
+            get { return claveUser; }
+            set { claveUser = value; }
+        }
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+        public string Apellido
+        {
+            get { return apellido; }
+            set { apellido = value; }
+        }
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+        public DateTime FechaNacimiento
+        {
+            get { return fechaNacimiento; }
+            set { fechaNacimiento = value; }
+        }
+        public DateTime FechaCreacion
+        {
+            get { return fechaCreacion; }
+            set { fechaCreacion = value; }
+        }
+        public int NumeroCel
+        {
+            get { return numerocel; }
+            set { numerocel = value; }
+        }
+        #endregion
+    }
+}
